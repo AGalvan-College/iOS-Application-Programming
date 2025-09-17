2d92485f357667e7d2dcce166ff35ab89799a414
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-09-17 09:45:54 -0500

    Exploring Constants and Variables

---
+++ constants_variables.swift
@@ -0,0 +1,20 @@
+#! /usr/bin/env swift
+
+// Examples of constants
+let theAnswerToTheUtimateQuestion = 42
+let pi = 3.14159
+let myName = "John Smith"
+
+// Examples of variables
+var currentTemperatureInCelcius = 27
+var myAge = 50
+var myLocation = "home"
+
+// Example of a Boolean constant
+let isRaining = true
+
+// Using type annotation to specify a type
+var restaurantRating: Double = 3
+
+// Type Safety (Cannot assign value of type 'String' to type 'Double')
+//restaurantRating = "Good"
