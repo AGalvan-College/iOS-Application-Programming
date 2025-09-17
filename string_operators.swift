a48473a90dafb00a672957f491dc0db574cd4194
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-09-17 10:21:12 -0500

    String Operators

---
+++ string_operators.swift
@@ -0,0 +1,13 @@
+#! /usr/bin/env swift
+
+// Performing string operations
+
+// Concatenate two strings using the '+' operator
+let greeting = "Good" + "Morning"
+
+// Cast an integer or real as a string or concatentate it with another string
+let rating = 3.5
+var ratingResult = "The resturanut rating is " + (String(rating))
+
+// String interpolation
+ratingResult = "The resturanut rating is \(rating)"
