47db12578594bc52531a2e57d4c627903a10d2ee
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-09-19 16:23:40 -0500

    Arithmetic Operators
    Update file

---
+++ arithmetic_operators.swift
@@ -0,0 +1,36 @@
+#! /usr/bin/env swift
+
+//  +   Addition
+//  -   Subtraction
+//  *   Multiplication
+//  /   Division
+
+// Operation with Numbers
+// (+) operator for Addition
+let sum = 23 + 20           // 43
+
+// (-) operator for Subtraction
+let result = 32 - sum       // -11
+
+// (*) operator for Multiplication
+let total = result * 5      // -55
+
+// (/) operator for Division
+let divide = total / 10     // -5
+
+// Operators can only work with the operands of the same type
+let a = 12
+let b = 12.0
+//let c = a + b   // Error: Binary operator '+' cannot be applied to operands of type 'Int' and 'Double'
+// To fix this.
+let c = Double(a) - b       // Making Integear A to Double
+let d = a - Int(b)          // Making Float B to Integear
+
+// Compound assignment Operators
+var aa = 1
+
+aa += 2
+// aa is now equal to 3
+
+aa -= 1
+// aa is now equal to 2
