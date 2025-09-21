834cf918946c8887db45a00d66dab26d5f568e0f
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-09-21 00:53:39 -0500

    Comparison Operators

---
+++ comparison_operators.swift
@@ -0,0 +1,18 @@
+#! /usr/bin/env swift
+
+// Using comparison Operators
+//  Operators       Description
+//  ==              Equal to
+//  !=              Not Equal to
+//  >               Greater than
+//  <               Less than
+//  >=              Greater than or Equal to
+//  <=              Less than or Equal to
+
+// Comparison Operators
+1 == 1  // Equal to,                    TRUE
+2 != 1  // Not Equal to,                TRUE
+2 > 1   // Greather than,               TRUE
+1 < 2   // Less than,                   TRUE
+1 >= 1  // Greater than or Equal to,    TRUE
+2 <= 1  // Less than or Equal to,       FALSE
