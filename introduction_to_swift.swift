0739f364033a1433c7eda047aa68d8d9cc779af2
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-09-26 23:31:57 -0500

    Intro to Swift

---
+++ introduction_to_swift.swift
@@ -0,0 +1,14 @@
+#! /usr/bin/env swift
+
+var greeting = "Hello, playground"
+greeting = "I can write in Swift!"
+print(greeting)
+
+var myInt: Int = 5
+print(myInt)
+
+var myDouble: Double = 0.5
+print(myDouble)
+
+var myBool: Bool = true
+print(myBool)
