35b9340403fbbb325abd85d118cc842fe2c36a76
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-09-26 23:23:37 -0500

    Build a War Card Game App
    (Xcode 16 Updated)

---
+++ xcode_lessons/War Card Game/War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by AG on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
