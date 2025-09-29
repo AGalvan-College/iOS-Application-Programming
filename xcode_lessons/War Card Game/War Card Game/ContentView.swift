cf9f990e0a207b8cbb811671f855df7ee70409de
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-09-29 09:58:05 -0500

    SwiftUI Buttons and Properties

--- xcode_lessons/War Card Game/War Card Game/ContentView.swift
+++ xcode_lessons/War Card Game/War Card Game/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var playerCard = "card7"
+    var cpuCard = "card13"
+    
+    var playerScore = 0
+    var cpuScore = 0
+    
     var body: some View {
         
         ZStack {
@@ -25,18 +32,23 @@ struct ContentView: View {
                 HStack {
                     Spacer()
                     
-                    Image("card2")
+                    Image(playerCard)
                     
                     Spacer()
                     
-                    Image("card3")
+                    Image(cpuCard)
                     
                     Spacer()
                 }
                 
                 Spacer()
                 
-                Image("button")
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+
                 
                 Spacer()
                 
@@ -48,7 +60,7 @@ struct ContentView: View {
                         Text("Player")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                     }
                     
@@ -58,7 +70,7 @@ struct ContentView: View {
                         Text("CPU")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                     }
                     
@@ -71,6 +83,11 @@ struct ContentView: View {
             }
         }
     }
+    
+    func deal(){
+        print("Deal cards")
+    }
+    
 }
 
 #Preview {
