bbb8d474fd21b86563a7a849375482c318fdb57f
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-09-29 08:25:28 -0500

    Swift Functions

---
+++ swift_functions.swift
@@ -0,0 +1,22 @@
+#! /usr/bin/env swift
+
+func sayHello(m: String) {
+    print(m)
+}
+
+sayHello(m: "I can code functions!")
+
+
+func doAdd(a: Int, b: Int) -> Int {
+    var sum = a + a + b + b
+    return sum
+}
+doAdd(a: 1, b: 1)
+
+func doAdd(_ a: Int, _ b: Int) -> Int {
+    var sum = a + b
+    return sum
+}
+
+let result = doAdd(3, 4)
+print(result)
