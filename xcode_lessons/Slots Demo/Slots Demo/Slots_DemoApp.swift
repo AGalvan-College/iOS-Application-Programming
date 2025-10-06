b67964bb4777bf6eff973b80d9f532dc3ea90d5c
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-10-05 20:16:01 -0500

    Build a Slot App Project

---
+++ xcode_lessons/Slots Demo/Slots Demo/Slots_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_DemoApp.swift
+//  Slots Demo
+//
+//  Created by AG on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Slots_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
