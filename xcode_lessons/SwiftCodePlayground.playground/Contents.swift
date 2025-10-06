0a9695048e31fafb613742bf76e77d2ea701836a
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-10-05 20:18:16 -0500

    Playground stuff
    stuff

---
+++ xcode_lessons/SwiftCodePlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting = "Hello, playground"
+greeting = "I can write in Swift!"
+print(greeting)
+
+var myInt: Int = 5
+print(myInt)
+
+var myDouble: Double = 0.5
+print(myDouble)
+
+var myBool: Bool = true
+print(myBool)
+
