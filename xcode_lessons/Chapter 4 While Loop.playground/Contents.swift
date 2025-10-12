54c8d4ea18e408e93c71cd54a3be4ee7f125b115
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-10-11 23:57:11 -0500

    Chapter 4 While Loop

---
+++ xcode_lessons/Chapter 4 While Loop.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+// While Loop
+
+//while confition == true {
+//    code
+//}
+
+// Condition is checked, and if true, the set of statements in the curly braces are executed
+
+var y = 0
+
+// y = 0 and until reach to '50'
+// when y reach to 45 and increase to 50 then it stops.
+while y < 50{
+    y += 5
+    print("y is \(y)")
+}
+
+var y1 = 100
+
+while y1 < 50{
+    y1 += 5
+    print("y1 is \(y1)")
+}
