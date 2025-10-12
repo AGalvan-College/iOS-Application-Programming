4b0e8b818bd9fca48bf518b5905797caae9f1c82
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-10-11 23:30:24 -0500

    Chapter 4 Range Operators

---
+++ xcode_lessons/Chapter 4 Range Operators.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// Range Operators and Loops
+// Department Store Program
+// Send Discount vouchers to customers between the ages of 18 and 38
+
+// For more information on range operators, visit:
+// https://docs.swift.org/swift-book/documentation/the-swift-programming-language/basicoperators/
+
+// firstNumber...lastNumber
+
+// Closed Range Operator
+let myRange = 10...20
+
+for Range in 10...20 {
+    print(Range)
+}
+
+print("**")
+
+// Half-open Range Operator
+let myRange2 = 10..<20
+
+// For this system
+for Range in 10..<20 {
+    print(Range)
+}
