5690bc6e935b90a72ad8a295c6c33b969ded96cb
AG <179846171+AGalvan-College@users.noreply.github.com>
2025-10-12 00:42:19 -0500

    Chapter 4 Repeat While Loop

---
+++ xcode_lessons/Chapter 4 Repeat While Loop.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//repeat {
+//    code
+//} // While condition == true
+
+// Repeat Loop
+// Set of statements is executed, then the condition is checked
+var x = 0
+repeat {
+    x += 5
+    print("x is \(x)")
+} while x < 50
+
